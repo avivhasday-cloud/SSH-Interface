e38333fb9009bff97fd35901686c53cb31b634e7
Aviv Hasday <avivhasday@gmail.com>
2022-07-29 13:10:28 +0300

    refactor camel case to snake case

--- gui/main_window.py
+++ gui/main_window.py
@@ -26,18 +26,18 @@ class MainWindowUI(QMainWindow):
         self.setCentralWidget(self.stack)
 
     def _input_is_valid(self):
-        return re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', self.form_connection_widget.ipEntry.text()) \
-               and len(self.form_connection_widget.userEntry.text()) != 0 \
-               and len(self.form_connection_widget.passwordEntry.text()) != 0
+        return re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', self.form_connection_widget.ip_entry.text()) \
+               and len(self.form_connection_widget.user_entry.text()) != 0 \
+               and len(self.form_connection_widget.password_entry.text()) != 0
 
     def on_connect_button_clicked(self):
         if self._input_is_valid():
-            self.ssh_client = SSHClient(ip=self.form_connection_widget.ipEntry.text(),
-                                        username=self.form_connection_widget.userEntry.text(),
-                                        password=self.form_connection_widget.passwordEntry.text())
+            self.ssh_client = SSHClient(ip=self.form_connection_widget.ip_entry.text(),
+                                        username=self.form_connection_widget.user_entry.text(),
+                                        password=self.form_connection_widget.password_entry.text())
             if self.ssh_client.is_connected():
                 self.form_connection_widget.btn1.setText("Disconnect")
-                self._clean_form([self.form_connection_widget.ipEntry, self.form_connection_widget.userEntry, self.form_connection_widget.passwordEntry])
+                self._clean_form([self.form_connection_widget.ip_entry, self.form_connection_widget.user_entry, self.form_connection_widget.password_entry])
                 self.main_window_widget.init_tab_views(self.ssh_client)
                 self.stack.setCurrentIndex(PAGE.MAIN_PAGE.value)
             else:
@@ -50,7 +50,7 @@ class MainWindowUI(QMainWindow):
 
     def setup_onclick_functions_to_buttons(self):
         self.main_window_widget.disconnect_button.clicked.connect(self.on_disconnect_button_clicked)
-        self.main_window_widget.file_system_widget.searchButton.clicked.connect(self.on_search_button_clicked)
+        self.main_window_widget.file_system_widget.search_button.clicked.connect(self.on_search_button_clicked)
 
     @staticmethod
     def _clean_form(line_edit_list: [QLineEdit]):
@@ -68,14 +68,14 @@ class MainWindowUI(QMainWindow):
         dialog_message.exec()
 
     def on_search_button_clicked(self):
-        remote_dir = self.main_window_widget.file_system_widget.searchEntry.text()
+        remote_dir = self.main_window_widget.file_system_widget.search_entry.text()
         if self.ssh_client.is_path_exists(remote_dir):
             file_stats_list = self.ssh_client.get_files_and_folders_stats_in_remote_dir(remote_dir)
             self.main_window_widget.file_system_widget.insert_file_stats_to_table(file_stats_list)
         else:
             dialog_message = DialogMessageBox("Path is not exists in remote server")
             dialog_message.exec()
-            self._clean_form([self.main_window_widget.file_system_widget.searchEntry])
+            self._clean_form([self.main_window_widget.file_system_widget.search_entry])
 
 
 def main():
